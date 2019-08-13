10e380717b89a6dc891595854c0fe3c6060a4fc3
jimmyD-GitHub <jamesalandixon@gmail.com>
2019-08-13 12:08:51 +0100

    Added app:refresh-ratings command for cron job.

---
+++ src/Command/RefreshBoxerRatings.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RefreshBoxerRatings extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'app:refresh-ratings';
+
+    protected function configure(): void
+    {
+        $this->setDescription('Refreshes the boxer ratings for each division.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln('TODO: Implement boxer ratings refresh.');
+    }
+}

93ea00655279f665fca1c7e53952f68d3b857ab6
jimmyD-GitHub <jamesalandixon@gmail.com>
2019-08-22 12:19:33 +0100

    Added extra assertions for refresh ratings job test.

--- tests/Command/RefreshBoxerRatingsTest.php
+++ tests/Command/RefreshBoxerRatingsTest.php
@@ -17,11 +17,27 @@ class RefreshBoxerRatingsTest extends KernelTestCase
         $commandTester = new CommandTester($command);
 
         $commandTester->execute([
-            'command'  => $command->getName(),
+            'command' => $command->getName(),
         ]);
 
         $output = $commandTester->getDisplay();
 
+        $this->assertContains('Finished updating boxer ratings for the heavyweight division.', $output);
+        $this->assertContains('No ratings updated for the cruiserweight division!', $output);
+        $this->assertContains('No ratings updated for the light-heavyweight division!', $output);
+        $this->assertContains('No ratings updated for the super-middleweight division!', $output);
+        $this->assertContains('No ratings updated for the middleweight division!', $output);
+        $this->assertContains('No ratings updated for the light-middleweight division!', $output);
+        $this->assertContains('No ratings updated for the welterweight division!', $output);
+        $this->assertContains('No ratings updated for the light-welterweight division!', $output);
+        $this->assertContains('No ratings updated for the lightweight division!', $output);
+        $this->assertContains('No ratings updated for the super-featherweight division!', $output);
+        $this->assertContains('No ratings updated for the featherweight division!', $output);
+        $this->assertContains('No ratings updated for the super-bantamweight division!', $output);
+        $this->assertContains('No ratings updated for the bantamweight division!', $output);
+        $this->assertContains('No ratings updated for the super-flyweight division!', $output);
+        $this->assertContains('No ratings updated for the flyweight division!', $output);
         $this->assertContains('No ratings updated for the light-flyweight division!', $output);
+        $this->assertContains('No ratings updated for the minimumweight division!', $output);
     }
 }

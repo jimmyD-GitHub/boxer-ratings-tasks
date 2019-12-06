1c8f3ba795eab872f96eb9b2e67f120278f2b7cb
jimmyD-GitHub <jamesalandixon@gmail.com>
2019-12-06 14:39:16 +0000

    Updated composer dependencies and doctrine config.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -31,7 +31,7 @@ class AppFixtures extends Fixture
 
         $finder = new Finder();
         $finder->in(__DIR__ . '/../../vendor/jimmyd-github/boxer-ratings-mysql/schema')
-            ->name('*.sql')
+            ->name(['boxers.sql', 'data.sql'])
             ->sortByName()
             ->files();
 
